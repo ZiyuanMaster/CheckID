b485b5adffa91c484557db7cf23b11d5e318b667
ZiyuanMaster <115977344+ZiyuanMaster@users.noreply.github.com>
2024-06-14 17:10:41 +0700

    Update NetEaseAPI.js

--- NetEaseAPI.js
+++ NetEaseAPI.js
@@ -1,1 +1,23 @@
+let UID;
+let SID;
 
+let xhr = new XMLHttpRequest();
+let url = `https://pay.neteasegames.com/gameclub/aceracer/${SID}/login-role?roleid=${UID}&client_type=gameclub`;
+
+xhr.open("POST", url, true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === this.DONE) {
+    let parsed = JSON.parse(xhr.responseText);
+    let IGN = parsed.data.rolename;
+
+    if (xhr.status === 200) {
+      console.log(IGN);
+    } else {
+      console.error("Request failed. Status: " + xhr.status);
+    }
+  }
+}
+
+function executeAPI() {
+  xhr.send();
+}
